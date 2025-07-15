7cb8e4686b9adea9953f8e4e7209be5c4e7cee22
Philipp Scheer <scheer28philipp@gmail.com>
2025-07-15 22:51:56 +0200

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import { createMDX } from 'fumadocs-mdx/next';
+
+const withMDX = createMDX();
+
+/** @type {import('next').NextConfig} */
+const config = {
+  reactStrictMode: true,
+  output: 'export',
+  basePath: '/philippscheer.github.io',
+  trailingSlash: true,
+};
+
+export default withMDX(config);
